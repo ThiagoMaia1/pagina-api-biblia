e54058001c74d0931689d990863cf0b2927a642e
Thiago Pereira Maia <tthiagopmaia@gmail.com>
2021-05-04 22:10:24 -0300

    Implements documentation in selected language (closes #6)

--- src/views/Documentation.vue
+++ src/views/Documentation.vue
@@ -1,8 +1,10 @@
 <template>
   <div class='view'>
     <Header imageUri="PileOfBooks.svg" :title="$t('navbar.documentation')" :subtitle="$t('documentation.subtitle')"/>
-    <VueMarkdown :source="docs">
-    </VueMarkdown>
+    <div id='content-container'>
+      <VueMarkdown :source="docs">
+      </VueMarkdown>
+    </div>
   </div>
 </template>
 
@@ -10,6 +12,12 @@
   import { Component, Vue } from 'vue-property-decorator';
   import Header from '@/components/Header/Header.vue'; 
   import VueMarkdown from 'vue-markdown'
+  import i18n from '@/translations'
+
+  const locales = {
+    'pt': 'Português',
+    'en': 'English',
+  }
 
   @Component({
     components: {
@@ -25,10 +33,39 @@
             Accept: "application/vnd.github.v3.full+json"
           }
         }).then(result => result.json())
-          .then(json => this.$data.docs = Buffer.from(json.content, 'base64').toString())
+          .then(json => {
+            let docs = Buffer.from(json.content, 'base64').toString()
+            docs = docs.split(/\n(?=[^\n]*\n=========)|\n=========\n\n/)
+            let locale = locales[i18n.locale]
+            console.log(locale);
+            for (let i = 0; i < docs.length; i++)
+              if (docs[i] === locale) {
+                this.$data.docs = docs[i+1]
+                return
+              }
+            this.$data.docs = docs[1]
+          })
     }
   })
   export default class Documentation extends Vue {
     docs = ''
   }
 </script>
+
+<style lang='scss'>
+  @import '@/style.scss'; 
+  #content-container {
+    width: 100%;
+    > * {
+      margin: 0 auto;
+      width: 60%;
+    }
+    text-align: justify;
+    h1 {
+      border-bottom: 1px solid #2c3e5063;
+    }
+    p {
+      margin-left: 2em;
+    }
+  }
+</style>
